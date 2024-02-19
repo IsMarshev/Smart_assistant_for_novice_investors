733eb5801d4f8d4f3f84ee685382d47b93c91c73
Илья Маршев <>
2024-02-19 11:50:43 +0300

    add chroma.py for run chroma db

---
+++ chroma.py
@@ -0,0 +1,28 @@
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+from chromadb.utils import embedding_functions
+from tqdm import tqdm
+import chromadb
+
+# client = chromadb.PersistentClient(path="/database/data")
+client = chromadb.HttpClient(host='localhost', port=8000)
+print('База данных подключена') 
+# invest_qa_emb = client.create_collection(name=db_name, metadata={'hnsw:space': 'cosine'})
+collection = client.create_collection(name="my_collection", embedding_function=embedding_functions.SentenceTransformerEmbeddingFunction(model_name="intfloat/multilingual-e5-small"), metadata={'hnsw:space': 'cosine'})
+print('Конфигурация коллекции создана')
+df = pd.read_excel('data/data.xlsx')
+collection.add(
+    documents=df.Answer.to_list(),
+    metadatas=[{'question': q} for q in df.Theme.to_list()],
+    ids=[str(i) for i in range(len(df))]
+)
+print('Данные загружены')
+# embeddddd = SentenceTransformer("intfloat/multilingual-e5-small")
+# query = "Что делать Если пришел маржин-колл"
+# input_em = embeddddd.encode(sentences=f"query: {query}", batch_size=1).tolist()
+
+# results = collection.query(
+#     query_embeddings=[input_em],
+#     n_results=5
+# )
+# print(results)
--- query_to_chroma.py
+++ query_to_chroma.py
@@ -1,7 +1,14 @@
-query = "Что делать Если пришел маржин-колл"
-input_em = model.encode(sentences=f"query: {query}", batch_size=1, device = device).tolist()
+import chromadb
+from sentence_transformers import SentenceTransformer
 
-results = invest_qa_emb.query(
+client = chromadb.HttpClient(host='localhost', port=8000)
+collection = client.get_collection("my_collection")
+
+embeddddd = SentenceTransformer("intfloat/multilingual-e5-small")
+query = "Чем отличается акции от облигаций"
+input_em = embeddddd.encode(sentences=f"query: {query}", batch_size=1).tolist()
+
+results = collection.query(
     query_embeddings=[input_em],
     n_results=5
 )
